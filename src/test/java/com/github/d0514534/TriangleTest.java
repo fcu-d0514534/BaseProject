ec6d1d6da4d40ca7c3935051455999bbcdf80a3b
D0514534 <D0514534@gmail.com>
2017-12-07 19:54:22 +0800

    create triangle

---
+++ src/test/java/com/github/d0514534/TriangleTest.java
@@ -0,0 +1,22 @@
+package com.github.d0514534;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void getType() throws Exception {
+        // Arrange
+        Triangle triangle = new Triangle(5, 10, 5);
+
+        // Act
+        String result = triangle.getType();
+
+        // Assert
+        Assert.assertEquals("不是三角形", result);
+    }
+
+
+}
